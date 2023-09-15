4cf92adc8bbbdbcf2cc15ef9f9376676f265c866
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-09-15 20:19:38 +0300

    #2 create server with simple html

--- www/index.js
+++ www/index.js
@@ -1,9 +1,24 @@
-const os = require('os')
-res_os = os.platform()
+const http = require('http')
 
-console.log(res_os)
 
-const math = require('./math')
-res = math.add(5,7)
+let server = http.createServer((req, res) => {
+  res.writeHead(200, {'content-type' : 'text/html; charset=utf-8'})
+  res.end(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Node JS</title>
+  </head>
+  <body>
+      <p>Hello, dog!</p>
+  </body>
+  </html>`)
+})
 
-console.log(res)
+const PORT = 3001
+const HOST = '127.0.0.1'
+
+server.listen(PORT, HOST, () => {
+  console.log(`Success: http://${HOST}:${PORT}`)
+})
