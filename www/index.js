23bb6b737413ff6a7f475e6cf73a50269397dcca
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-09-15 12:25:48 +0300

    #1 added using modules to project

--- www/index.js
+++ www/index.js
@@ -1,3 +1,9 @@
-let num = 5
+const os = require('os')
+res_os = os.platform()
 
-console.log(`Var = ${num}`)
+console.log(res_os)
+
+const math = require('./math')
+res = math.add(5,7)
+
+console.log(res)
